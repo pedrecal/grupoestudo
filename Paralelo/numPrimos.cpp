25433e063cb0c97029325b9e40ddd4464b3a42a4
matAlmeida <mat.almeida@live.com>
2016-02-17 13:00:52 -0300

    Numeros primos funcionando em paralelo

--- Paralelo/numPrimos.cpp
+++ Paralelo/numPrimos.cpp
@@ -1,24 +1,46 @@
-// icpc numPrimos.cpp -o -lm numPrimos.o
+// icpc -openmp numPrimos.cpp -o  numPrimos.o
 #include <iostream>
-#include <math.h>
+#include <time.h>
+#include <omp.h>
 
 using namespace std;
 
-void numerosPrimos();
+int numerosPrimos(int);
 
-void numerosPrimos2();
+int numerosPrimosParalelo(int);
 
 int main(int argc, char const *argv[]) {
+    int max = 100000000;
+    double startTime = 0.0;
+    double endTime = 0.0;
+    double tempo1 = 0.0;
+    double tempo2 = 0.0;
+    int funcResult1;
+    int funcResult2;
 
-    numerosPrimos();
+    cout << "------------------------------ Serial ------------------------------" << endl;
+    startTime = clock()/CLOCKS_PER_SEC;
+    funcResult1 = numerosPrimos(max);
+    endTime = clock()/CLOCKS_PER_SEC;
+    tempo1 = endTime - startTime;
+
+    cout << endl << "Total numeros primos: " << funcResult1 << " --- Tempo Serial: "<< tempo1 << endl << endl;
+
+    cout << "----------------------------- Parallel -----------------------------" << endl;
+    startTime = omp_get_wtime();
+    funcResult2 = numerosPrimosParalelo(max);
+    endTime = omp_get_wtime();
+    tempo2 = endTime - startTime;
+
+    cout << endl << "Total numeros primos: " << funcResult2 <<" --- Tempo Paralelo: "<< tempo2 << endl;
 
     return 0;
 }
 
-void numerosPrimos()
+int numerosPrimos(int max)
 {
-    int max = 100;
     bool flag = true;
+    int numeros = 0;
 
     for(int i = 1; i < max; i++)
     {
@@ -32,9 +54,45 @@ void numerosPrimos()
         }
 
         if(flag)
-            cout << " " << i;
+        {
+            numeros++;
+            //cout << " " << i;
+            //cout << " . ";
+        }
 
         flag = true;
     }
     cout << endl;
+
+    return numeros;
+}
+
+int numerosPrimosParalelo(int max)
+{
+    bool flag = true;
+    int numeros = 0;
+
+    #pragma omp parallel for shared(numeros)
+        for(int i = 1; i < max; i++)
+        {
+            for(int j = 2; j < i; j++)
+            {
+                if(i % j == 0)
+                {
+                    flag = false;
+                    break;
+                }
+            }
+
+            if(flag)
+            {
+                numeros++;
+                //cout << " " << i;
+                //cout << " . ";
+            }
+
+            flag = true;
+        }
+
+    return numeros;
 }
